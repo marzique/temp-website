697dc3e82ae7a02689173ccba0b16f8145738477
tarn <marzique@gmail.com>
2020-10-28 15:53:52 +0200

    cool column django admin prefiction expanded

--- apps/forecasts/admin.py
+++ apps/forecasts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from forecasts.models import Forecast, Season, Fixture, Prediction
 
@@ -29,5 +30,19 @@ class FixtureAdmin(admin.ModelAdmin):
 
 @admin.register(Prediction)
 class PredictionAdmin(admin.ModelAdmin):
-    list_display = ['user', 'forecast', 'results', 'created_at']
+    list_display = ['user', 'forecast', 'with_teams', 'created_at']
     readonly_fields = ['created_at', ]
+
+
+    def with_teams(self, obj):
+        new_results = []
+        
+        for week, goals in obj.results.items():
+            fixture = Fixture.objects.get(id=week)
+            home = fixture.get_home_name()
+            guest = fixture.get_guest_name()
+            new_results.append(
+                f'{home} {goals[0]}:{goals[1]} {guest}'
+            )
+            
+        return format_html('<br>'.join(new_results))

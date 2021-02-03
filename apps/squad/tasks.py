0bc85e75fb71d189369a375bf8fe594bbfbec86a
Denys Tarn <denys.tarnavskyi@wesoftyou.com>
2021-02-03 14:47:29 +0200

    photo as attachment

--- apps/squad/tasks.py
+++ apps/squad/tasks.py
@@ -11,10 +11,9 @@ bot = TelegramBot()
 @shared_task
 def notify_birthdays():
     for player in get_todays_birthday_players():
-        text = f"""
-        Від імені команди вітаємо {player.full_name} з днем народження!
-        {player.first_name} святкує {player.years}річчя!
-        https://tempfc.club{player.photo.url}
-        """
-        bot.send_text(text)
+        text = f"\
+        Від імені команди вітаємо {player.full_name} з днем народження!\
+        {player.first_name} святкує {player.years}річчя!"
+        photo_urls = [f'https://tempfc.club{player.photo.url}']
+        bot.send_album(photo_urls=photo_urls, text=text)
 

29ab39576b78e8c5686af136c99ea069363a2a85
Denys Tarn <denys.tarnavskyi@wesoftyou.com>
2021-02-25 02:03:50 +0200

    fix leagues test

--- apps/main/tests/test_main_page.py
+++ apps/main/tests/test_main_page.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth.models import User
 from core.tests import BaseClassTestCase
+from scoreboard.models import League
 
 
 class MainPageTestCase(BaseClassTestCase):
@@ -10,7 +11,8 @@ class MainPageTestCase(BaseClassTestCase):
         self.url = self.reverse('index')
         self.admin = User.objects.create_superuser('admin', 'admin@test.com', 's3cr37')
         self.user = User.objects.create_user('user', 'user@test.com', 'qwerty')
-    
+        self.league = League.objects.create(name='test', years='2020')
+
     def test_page_response(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
